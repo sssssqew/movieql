ce354238e7bb87b43ceb6f5982aca3553b0548d0
BuildTools <unconfigured@null.spigotmc.org>
2019-07-12 21:16:15 +0800

    영화 API 초기버전 완성

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,50 @@
-let movies = [
-  {
-    id: 0,
-    name: "Star Wars - The new one",
-    score: 1
-  },
-  {
-    id: 1,
-    name: "Avengers - The new one",
-    score: 8
-  },
-  {
-    id: 2,
-    name: "The godfather 1",
-    score: 99
-  },
-  {
-    id: 3,
-    name: "Logan",
-    score: 2
-  },
-]
+import axios from 'axios'
+const BASE_URL = 'https://yts.lt/api/v2/'
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`
 
-export const getMovies = () => movies;
+const API_URL = 'https://yts.lt/api/v2/list_movies.json?'
 
-export const getById = id => {
-  const filteredMovies = movies.filter(movie => movie.id === id)
-  return filteredMovies[0]
+
+
+export const getMovies = async (limit, rating) =>  {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating
+    }
+  })
+  return movies
 }
 
-export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id !== id)
-  if(movies.length > cleanedMovies.length){
-    movies = cleanedMovies;
-    return true
-  }else{
-    return false
-  }
+
+export const getMovie = async id => {
+  const {
+    data: {
+      data: { movie }
+    }
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id
+    }
+  })
+  return movie
 }
 
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score,
-  }
-  movies.push(newMovie)
-  return newMovie
+export const getSuggestions = async id => {
+  const { 
+    data: {
+      data: { movies }
+    }
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id
+    }
+  })
+  return movies
 }
